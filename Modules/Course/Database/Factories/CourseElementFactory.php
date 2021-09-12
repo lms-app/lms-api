581bf676a2e687e5a07a48cdf566a4e3c284effa
Igor <i.sinitsyn@look.pet>
2021-09-12 19:38:19 +0300

    fix namespace

--- Modules/Course/Database/Factories/CourseElementFactory.php
+++ Modules/Course/Database/Factories/CourseElementFactory.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Modules\Course\Database\factories;
+namespace Modules\Course\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- Modules/File/Database/Factories/FileFactory.php
+++ Modules/File/Database/Factories/FileFactory.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Modules\File\Database\factories;
+namespace Modules\File\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
