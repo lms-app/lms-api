95a3570f7b3130c503da06e399db989edb724f7b
Igor <i.sinitsyn@look.pet>
2021-09-25 14:26:21 +0300

    add count catalog

--- Modules/Course/Http/Responses/CourseDataResponse.php
+++ Modules/Course/Http/Responses/CourseDataResponse.php
@@ -6,6 +6,18 @@ namespace Modules\Course\Http\Responses;
 use Illuminate\Http\JsonResponse;
 use Modules\Course\Entities\Course;
 
+/**
+ * @OA\Schema(
+ *   schema="Course.DataResponse",
+ *   type="object",
+ *   @OA\Property(property="title", type="string", description="Название курса", example="Курс по космонавтике"),
+ *   @OA\Property(property="status", type="string", description="Статус курса (открытый, закрытый, ограниченный)", example="open"),
+ *   @OA\Property(property="short_description", type="string", description="Краткое описание курса", example=""),
+ *   @OA\Property(property="description", type="string", description="Полное описание курса", example=""),
+ *   @OA\Property(property="author_id", type="integer", description="ID пользователя", example="10"),
+ *   @OA\Property(property="folder_id", type="integer", description="ID папки, по умолчанию null", example="100"),
+ * )
+ */
 final class CourseDataResponse extends JsonResponse
 {
     public static function get(Course $course):self
