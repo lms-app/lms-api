809e32ebe5a7c1ef393c837e76bc0aae88d6ed1f
Igor <i.sinitsyn@look.pet>
2021-09-25 14:29:16 +0300

    tests and response

--- Modules/Test/Database/Migrations/2021_09_15_195257_create_test_sections_table.php
+++ Modules/Test/Database/Migrations/2021_09_15_195257_create_test_sections_table.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-final class CreateTestSectionTable extends Migration
+final class CreateTestSectionsTable extends Migration
 {
     public function up():void
     {
